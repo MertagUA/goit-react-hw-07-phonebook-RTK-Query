b0a97d47f356645d1dc9ec9679e2e247d7b2d38c
Viacheslav Kobets <mertag_nu@ukr.net>
2023-04-20 11:09:53 +0300

    Update contactsSlice.js

--- src/redux/Slices/contactsSlice.js
+++ src/redux/Slices/contactsSlice.js
@@ -1,36 +1,3 @@
-// import { createSlice } from '@reduxjs/toolkit';
-// import { persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-
-// const contactsSlice = createSlice({
-//   name: 'contacts',
-//   initialState: { contacts: [] },
-//   reducers: {
-//     add: (state, action) => {
-//       state.contacts.push(action.payload);
-//     },
-//     remove: (state, action) => {
-//       state.contacts = state.contacts.filter(
-//         contact => contact.id !== action.payload
-//       );
-//     },
-//   },
-// });
-
-// const persistConfig = {
-//   key: 'contacts',
-//   storage,
-// };
-
-// export const contactsReducer = persistReducer(
-//   persistConfig,
-//   contactsSlice.reducer
-// );
-
-// export const { add, remove } = contactsSlice.actions;
-
-// export const getContacts = state => state.savedContacts.contacts;
-
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const contactsApi = createApi({
